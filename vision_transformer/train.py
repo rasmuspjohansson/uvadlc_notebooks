57fd5b59fd70483cc46b0d3902e2049797dd4083
Rasmus_Johansson <rajoh@sdfi.dk>
2022-09-21 15:02:48 +0200

    mada a python module for training a vision transformer on the CIFAR10 dataset

---
+++ vision_transformer/train.py
@@ -0,0 +1,132 @@
+## Standard libraries
+import os
+import numpy as np
+import random
+import math
+import json
+from functools import partial
+from PIL import Image
+
+## Imports for plotting
+import matplotlib.pyplot as plt
+plt.set_cmap('cividis')
+# %matplotlib inline
+from IPython.display import set_matplotlib_formats
+set_matplotlib_formats('svg', 'pdf') # For export
+from matplotlib.colors import to_rgb
+import matplotlib
+matplotlib.rcParams['lines.linewidth'] = 2.0
+import seaborn as sns
+sns.reset_orig()
+
+## tqdm for loading bars
+from tqdm.notebook import tqdm
+
+## PyTorch
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.data as data
+import torch.optim as optim
+
+## Torchvision
+import torchvision
+from torchvision.datasets import CIFAR10
+from torchvision import transforms
+
+# PyTorch Lightning
+import pytorch_lightning as pl
+
+from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
+
+# Import tensorboard
+# %load_ext tensorboard
+
+# Path to the folder where the datasets are/should be downloaded (e.g. CIFAR10)
+DATASET_PATH = "../data"
+# Path to the folder where the pretrained models are saved
+CHECKPOINT_PATH = "../saved_models/tutorial15"
+
+# Setting the seed
+pl.seed_everything(42)
+
+# Ensure that all operations are deterministic on GPU (if used) for reproducibility
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+
+device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
+print("Device:", device)
+
+import model as model_lib
+
+
+def train_model(**kwargs):
+    trainer = pl.Trainer(default_root_dir=os.path.join(CHECKPOINT_PATH, "ViT"),
+                         gpus=1 if str(device)=="cuda:0" else 0,
+                         max_epochs=180,
+                         callbacks=[ModelCheckpoint(save_weights_only=True, mode="max", monitor="val_acc"),
+                                    LearningRateMonitor("epoch")])
+    trainer.logger._log_graph = True         # If True, we plot the computation graph in tensorboard
+    trainer.logger._default_hp_metric = None # Optional logging argument that we don't need
+
+    # Check whether pretrained model exists. If yes, load it and skip training
+    pretrained_filename = os.path.join(CHECKPOINT_PATH, "ViT.ckpt")
+    if os.path.isfile(pretrained_filename):
+        print(f"Found pretrained model at {pretrained_filename}, loading...")
+        model = model_lib.ViT.load_from_checkpoint(pretrained_filename) # Automatically loads the model with the saved hyperparameters
+    else:
+        pl.seed_everything(42) # To be reproducable
+        model = model_lib.ViT(**kwargs,train_loader=train_loader)
+        trainer.fit(model, train_loader, val_loader)
+        model = ViT.load_from_checkpoint(trainer.checkpoint_callback.best_model_path) # Load best checkpoint after training
+
+    # Test best model on validation and test set
+    val_result = trainer.test(model, val_loader, verbose=False)
+    test_result = trainer.test(model, test_loader, verbose=False)
+    result = {"test": test_result[0]["test_acc"], "val": val_result[0]["test_acc"]}
+
+    return model, result
+
+
+
+test_transform = transforms.Compose([transforms.ToTensor(),
+                                     transforms.Normalize([0.49139968, 0.48215841, 0.44653091], [0.24703223, 0.24348513, 0.26158784])
+                                     ])
+# For training, we add some augmentation. Networks are too powerful and would overfit.
+train_transform = transforms.Compose([transforms.RandomHorizontalFlip(),
+                                      transforms.RandomResizedCrop((32,32),scale=(0.8,1.0),ratio=(0.9,1.1)),
+                                      transforms.ToTensor(),
+                                      transforms.Normalize([0.49139968, 0.48215841, 0.44653091], [0.24703223, 0.24348513, 0.26158784])
+                                     ])
+# Loading the training dataset. We need to split it into a training and validation part
+# We need to do a little trick because the validation set should not use the augmentation.
+train_dataset = CIFAR10(root=DATASET_PATH, train=True, transform=train_transform, download=True)
+val_dataset = CIFAR10(root=DATASET_PATH, train=True, transform=test_transform, download=True)
+pl.seed_everything(42)
+train_set, _ = torch.utils.data.random_split(train_dataset, [45000, 5000])
+pl.seed_everything(42)
+_, val_set = torch.utils.data.random_split(val_dataset, [45000, 5000])
+
+# Loading the test set
+test_set = CIFAR10(root=DATASET_PATH, train=False, transform=test_transform, download=True)
+
+# We define a set of data loaders that we can use for various purposes later.
+train_loader = data.DataLoader(train_set, batch_size=128, shuffle=True, drop_last=True, pin_memory=True, num_workers=4)
+val_loader = data.DataLoader(val_set, batch_size=128, shuffle=False, drop_last=False, num_workers=4)
+test_loader = data.DataLoader(test_set, batch_size=128, shuffle=False, drop_last=False, num_workers=4)
+
+
+
+model, results = train_model(model_kwargs={
+                                'embed_dim': 256,
+                                'hidden_dim': 512,
+                                'num_heads': 8,
+                                'num_layers': 6,
+                                'patch_size': 4,
+                                'num_channels': 3,
+                                'num_patches': 64,
+                                'num_classes': 10,
+                                'dropout': 0.2
+                            },
+                            lr=3e-4)
+print("ViT results", results)
